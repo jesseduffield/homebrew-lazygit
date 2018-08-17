2b13c97a4dc0141b0c4b331c836820bda473c79b
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-17 11:16:36 +1000

    Brew formula update for lazygit version v0.1.64

--- lazygit.rb
+++ lazygit.rb
@@ -1,9 +1,9 @@
 class Lazygit < Formula
   desc "A simple terminal UI for git commands, written in Go"
   homepage "https://github.com/jesseduffield/lazygit/"
-  url "https://github.com/jesseduffield/lazygit/releases/download/v0.1.61/lazygit_0.1.61_Darwin_x86_64.tar.gz"
-  version "0.1.61"
-  sha256 "febd6ef225e52c8d811cb64b2d1470f416205d01c5996fb35d26c0313d59ca0e"
+  url "https://github.com/jesseduffield/lazygit/releases/download/v0.1.64/lazygit_0.1.64_Darwin_x86_64.tar.gz"
+  version "0.1.64"
+  sha256 "5c4a8f2edcb36e32206736ecff5102418ccce7b8338f9a14d9735c807fd92502"
 
   def install
     bin.install "lazygit"
