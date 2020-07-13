4d8b0325ba286001342bf4791b5e20c6955d478a
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-14 09:52:03 +1000

    Brew formula update for lazygit version v0.20.8

--- lazygit.rb
+++ lazygit.rb
@@ -2,24 +2,24 @@
 class Lazygit < Formula
   desc "A simple terminal UI for git commands, written in Go"
   homepage "https://github.com/jesseduffield/lazygit/"
-  version "0.20.6"
+  version "0.20.8"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/jesseduffield/lazygit/releases/download/v0.20.6/lazygit_0.20.6_Darwin_x86_64.tar.gz"
-    sha256 "2fc9ac9ec2de16ba31956260f48b1ea7003cf5c414d1b099d12c5aa0687733f2"
+    url "https://github.com/jesseduffield/lazygit/releases/download/v0.20.8/lazygit_0.20.8_Darwin_x86_64.tar.gz"
+    sha256 "dafe12c9b76e4651dbc93cee849fd80abd1ebf97c5c650073d4aed1083136e27"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/jesseduffield/lazygit/releases/download/v0.20.6/lazygit_0.20.6_Linux_x86_64.tar.gz"
-      sha256 "95e2e4224f8c135d72943fc8c3ae73f24eb790aa7803af082fd426e4a79bd34c"
+      url "https://github.com/jesseduffield/lazygit/releases/download/v0.20.8/lazygit_0.20.8_Linux_x86_64.tar.gz"
+      sha256 "080de69425d858b14af0e6d34db8549b0321131d92f18a2e45fbd03f4f903b68"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/jesseduffield/lazygit/releases/download/v0.20.6/lazygit_0.20.6_Linux_arm64.tar.gz"
-        sha256 "4412a49f93331e4c9e91698801a260ae182873eafa0148f8c869514acd18f9c1"
+        url "https://github.com/jesseduffield/lazygit/releases/download/v0.20.8/lazygit_0.20.8_Linux_arm64.tar.gz"
+        sha256 "bb035f2613196e72da78df944ee0082326e66f4df28dde86c10d578548273832"
       else
-        url "https://github.com/jesseduffield/lazygit/releases/download/v0.20.6/lazygit_0.20.6_Linux_armv6.tar.gz"
-        sha256 "be5fc2ac2b38580dd2c3fe0be8233962088740d7fca21780c9329bae0e5712f7"
+        url "https://github.com/jesseduffield/lazygit/releases/download/v0.20.8/lazygit_0.20.8_Linux_armv6.tar.gz"
+        sha256 "c303c07cc592b33e089f7a757ed20cf340bbe1392ae7a4225ef24bed2e978c98"
       end
     end
   end
